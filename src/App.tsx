40578517051d9666a80c786294519a79f93c523a
Robin Stumpf <rstumpf@3-F-2.42heilbronn.de>
2024-11-19 11:47:45 +0100

    tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { useState } from 'react'
-import './App.css'
 import { BrowserRouter, Route, Routes} from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import HomePage from './pages/HomePage';
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,9 +1,12 @@
-
+import { Button } from "@/components/ui/button"
 
 
 export default function HomePage(){
-
     return(
-        <p>Homepage</p>
-    )
+		<div>
+			        <p className="bg-red-400">Homepage</p>
+		<Button> Hi</Button>
+		</div>
+
+	)
 }
