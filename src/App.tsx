6e5e0e73be616566b92e1d6c53e609cd8c83b3a4
Amme <anne-sophie.nehls@t-online.de>
2024-11-19 15:51:47 +0100

    Lauyout in den Router eingebunden

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,35 @@
-import { useState } from 'react'
-import { BrowserRouter, Route, Routes} from 'react-router-dom'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import HomePage from './pages/HomePage';
-import LoginPage from './pages/LoginPage';
-import DetailsPage from './pages/DetailsPage';
-import RentPage from './pages/RentPage';
-import BookingPage from './pages/BookingPage';
-import FavoritesPage from './pages/FavoritesPage';
-import ProfilePage from './pages/ProfilePage';
+import { useState } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import DetailsPage from "./pages/DetailsPage";
+import RentPage from "./pages/RentPage";
+import BookingPage from "./pages/BookingPage";
+import FavoritesPage from "./pages/FavoritesPage";
+import ProfilePage from "./pages/ProfilePage";
+import Layout from "./components/Layout";
 
 const client = new QueryClient();
 
 function App() {
-return (
-  <QueryClientProvider client={client}>
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<HomePage/>}/>
-        <Route path='/details' element={<DetailsPage/>}/>
-        <Route path='/bookings' element={<BookingPage/>}/>
-        <Route path='/favorites/:id' element={<FavoritesPage/>}/>
-        <Route path='/profile/:id' element={<ProfilePage/>}/>
-        <Route path='/rent/:id' element={<RentPage/>}/>
-        <Route path='/login' element={<LoginPage/>}/>       
-      </Routes>
-    </BrowserRouter>
-  </QueryClientProvider>
-)
+  return (
+    <QueryClientProvider client={client}>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<Layout />}>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/details" element={<DetailsPage />} />
+            <Route path="/bookings" element={<BookingPage />} />
+            <Route path="/favorites/:id" element={<FavoritesPage />} />
+            <Route path="/profile/:id" element={<ProfilePage />} />
+            <Route path="/rent/:id" element={<RentPage />} />
+            <Route path="/login" element={<LoginPage />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
+  );
 }
 
-export default App
+export default App;
