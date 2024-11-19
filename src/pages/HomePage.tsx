f24dcdd0f167d3eca4cb170af55f3daae1269294
Robin Stumpf <rstumpf@3-F-2.42heilbronn.de>
2024-11-19 12:47:45 +0100

    small changes

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,12 +1,10 @@
 import { Button } from "@/components/ui/button"
 
-
 export default function HomePage(){
     return(
 		<div>
-			        <p className="bg-red-400">Homepage</p>
-		<Button> Hi</Button>
+			<p className="bg-red-400">Homepage</p>
+			<Button> Hi</Button>
 		</div>
-
 	)
 }
