53597c9cf9159010074b362386075a68d4019845
Amme <anne-sophie.nehls@t-online.de>
2024-11-20 14:51:43 +0100

    public img links anders ausprobiert

--- src/components/CarAddOne.tsx
+++ src/components/CarAddOne.tsx
@@ -3,7 +3,7 @@ import { Button } from "./ui/button";
 
 export default function CarAddOne() {
   return (
-    <div className="text-white h-80	rounded-md bg-[url('public/img/Background-Ads1.svg')] p-10 m-4">
+    <div className="text-white h-80	rounded-md bg-[url('/img/Background-Ads1.svg')] p-10 m-4">
       <h2 className="text-3xl w-1/2	">The Best Platform for Car Rental</h2>
       <p className="py-5 w-1/2	">
         Providing cheap car rental services and safe and comfortable facilities.
@@ -12,7 +12,7 @@ export default function CarAddOne() {
         <Link to="/rent/:id">Rental Car</Link>
       </Button>
       <img
-        src="../../public/img/autoAdd1.png"
+        src="/img/autoAdd1.png"
         alt=""
         className="-mt-16 pl-6 w-3/4 slide-right"
       />
--- src/components/CarAddTow.tsx
+++ src/components/CarAddTow.tsx
@@ -3,7 +3,7 @@ import { Button } from "./ui/button";
 
 export default function CarAddTwo() {
   return (
-    <div className="text-white h-80	 rounded-md bg-[url('../../public/img/Background-Ads2.svg')] p-10 pb-10 m-4">
+    <div className="text-white h-80	 rounded-md bg-[url('/img/Background-Ads2.svg')] p-10 pb-10 m-4">
       <h2 className="text-3xl w-1/2	">Easy way to rent a car at a low price</h2>
       <p className="py-5  w-1/2	">
         Ease of doing a car rental safely and reliably. Of course at a low
@@ -13,7 +13,7 @@ export default function CarAddTwo() {
         <Link to="/rent/:id">Rental Car</Link>
       </Button>
       <img
-        src="../../public/img/autoAdd2.png"
+        src="/img/autoAdd2.png"
         alt=""
         className="pl-20 w-4/5	-mt-12 slide-right"
       />
