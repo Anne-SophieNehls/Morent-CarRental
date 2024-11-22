c7a33a2888b811b83ca816b937e4d8fc106fe371
Amme <anne-sophie.nehls@t-online.de>
2024-11-22 09:20:02 +0100

    werbung kleine korr

--- src/components/CarAddTow.tsx
+++ src/components/CarAddTow.tsx
@@ -4,8 +4,8 @@ import { Button } from "./ui/button";
 export default function CarAddTwo() {
   return (
     <div className="text-white h-80	 rounded-md bg-[url('/img/Background-Ads2.svg')] p-10">
-      <h2 className="text-3xl w-1/2	">Easy way to rent a car at a low price</h2>
-      <p className="py-5 w-1/2">
+      <h2 className="text-3xl w-3/5	">Easy way to rent a car at a low price</h2>
+      <p className="py-5 w-2/3 pb-8">
         Ease of doing a car rental safely and reliably. Of course at a low
         price.
       </p>
