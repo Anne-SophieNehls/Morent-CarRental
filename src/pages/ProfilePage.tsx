e46738aa4b21a87c3e1863fe8489cdbf5394fec1
Amme <anne-sophie.nehls@t-online.de>
2024-11-26 16:14:16 +0100

    profil

--- src/pages/ProfilePage.tsx
+++ src/pages/ProfilePage.tsx
@@ -1,5 +1,12 @@
-export default function ProfilePage(){
-    return(
-        <p>My Profile</p>
-    )
+export default function ProfilePage() {
+  return (
+    <div>
+      {" "}
+      <p>My Profile</p>
+      <p>name</p>
+      <p>nachname</p>
+      <p></p>
+      <p></p>
+    </div>
+  );
 }
