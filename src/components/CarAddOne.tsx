66f08c77daa26e5974950c0355232990b1ca3339
Amme <anne-sophie.nehls@t-online.de>
2024-11-26 16:45:13 +0100

    d

--- src/components/CarAddOne.tsx
+++ src/components/CarAddOne.tsx
@@ -4,7 +4,7 @@ import { Button } from "./ui/button";
 export default function CarAddOne() {
   return (
     <div
-      className={`text-white h-80	rounded-md bg-[url('img/Background-Ads1.png')] z-0 p-10 `}
+      className={`text-white h-80	rounded-md bg-[url('/img/Background-Ads1.png')] z-0 p-10 `}
     >
       <h2 className="text-3xl w-3/5	">The Best Platform for Car Rental</h2>
       <p className="my-5 w-2/3	">
