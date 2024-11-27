1cba4330239fbe728d19d7adf4a326c612b9155e
Amme <anne-sophie.nehls@t-online.de>
2024-11-27 12:10:34 +0100

    padding

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -6,17 +6,16 @@ import { useThemeContext } from "@/context/LightDarkModeContext";
 export default function Layout() {
   const { theme } = useThemeContext();
 
-
   return (
     <div className={`theme--${theme}-hf h-screen flex flex-col`}>
       <div>
         <Header />
       </div>
-      
-      <main className={`flex-1 bg-[#F6F7F9] ${`theme--${theme}-bg`}`}>
+
+      <main className={`flex-1 p-3 bg-[#F6F7F9] ${`theme--${theme}-bg`}`}>
         <Outlet />
       </main>
-      
+
       <div>
         <Footer />
       </div>
