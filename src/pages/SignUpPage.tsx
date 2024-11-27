42f21ad43702adb12c136a7518ee5f0de4d34158
SteveDwumah <steve.dwmh@outlook.de>
2024-11-27 09:15:59 +0100

    update

--- src/pages/SignUpPage.tsx
+++ src/pages/SignUpPage.tsx
@@ -4,11 +4,8 @@ import imageIcon from "../../public/img/icons/image-upload-icon.svg";
 import { useRef, useState } from "react";
 import { useUserContext } from "@/context/userContext";
 import { supabase } from "@/lib/supabase";
-import { useThemeContext } from "@/context/LightDarkModeContext";
 
 export default function SignUpPage() {
-  const { theme } = useThemeContext();
-
   const [email, setEmail] = useState("");
   const [firstName, setFirstname] = useState("");
   const [lastName, setLastname] = useState("");
@@ -51,6 +48,64 @@ export default function SignUpPage() {
   };
 
   return (
+    <section className="sm:w-screen flex justify-center">
+      <div className="w-96 p-3 bg-white rounded-lg flex flex-col items-center justify-center m-8">
+        <h1 className="font-semibold text-2xl text-center mb-7">
+          Neuen Account anlegen
+        </h1>
+        <div>
+          <form className="flex flex-col gap-4 px-5" onSubmit={handleSubmit}>
+            <div>
+              <label htmlFor="E-mail">E-mail</label>
+              <Input
+                className="rounded-full w-4/5"
+                type="text"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
+            <div>
+              <label className="" htmlFor="E-mail">
+                Password
+              </label>
+              <Input
+                className="rounded-full w-4/5"
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+            <div>
+              <label htmlFor="E-mail">First name</label>
+              <Input
+                className="rounded-full w-4/5"
+                type="text"
+                value={firstName}
+                onChange={(e) => setFirstname(e.target.value)}
+              />
+            </div>
+            <div>
+              <label htmlFor="E-mail">Last name</label>
+              <Input
+                className="rounded-full w-4/5"
+                type="text"
+                value={lastName}
+                onChange={(e) => setLastname(e.target.value)}
+              />
+            </div>
+            <div>
+              <label>Profile picture</label>
+              <Input
+                className="rounded-full w-4/5"
+                type="file"
+                src=""
+                alt={` Image-Upload`}
+                ref={fileRef}
+              />
+            </div>
+            <Button className="bg-[#3563E9] mt-5 w-full">Sign up</Button>
+          </form>
+        </div>
       </div>
     </section>
   );
