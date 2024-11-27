c2070b0b2b80dbff1e14b95a9fc50e087392cbe4
Amme <anne-sophie.nehls@t-online.de>
2024-11-27 18:36:51 +0100

    läuft endlich

--- src/pages/ProfilePage.tsx
+++ src/pages/ProfilePage.tsx
@@ -19,7 +19,7 @@ interface Profile {
   id: string;
   first_name: string;
   last_name: string;
-  image_url: string;
+  image_url: string | null;
 }
 
 export default function ProfilePage() {
@@ -40,9 +40,9 @@ export default function ProfilePage() {
         if (error) {
           console.error("Error loading user profile:", error);
         } else {
+          setProfile(data);
           return data;
         }
-        setProfile(data!);
       }
     };
 
@@ -95,22 +95,29 @@ export default function ProfilePage() {
 
   return (
     <div
-      className={`w-96 p-3 bg-white shadow-sm ${`theme--${theme}-card`} rounded-lg`}
+      className={`w-96 p-3 bg-white shadow-sm ${`theme--${theme}-card`} flex flex-col gap-5 m-auto p-8 rounded-lg`}
     >
-      <h2>My Profile</h2>
+      <h2 className="font-semibold text-2xl text-center mb-7">My Profile</h2>
       {/*  <img
         src={imageLink || `/img/icons/profile-without-profilpictuare.svg`}
         alt="Profile Picture"
       /> */}
-      <p>First Name: {profile?.first_name}</p>
-      <p>Last Name: {profile?.last_name}</p>
-      <p>Email: {user?.email}</p>
+      <p>
+        <span className="font-semibold ">First Name:</span>{" "}
+        {profile?.first_name}
+      </p>
+      <p>
+        <span className="font-semibold ">Last Name: </span> {profile?.last_name}
+      </p>
+      <p>
+        <span className="font-semibold ">Email: </span> {user?.email}
+      </p>
       <div>
         <Link to="/favorites">
-          <Button>Your Favorites</Button>
+          <Button className="bg-[#3563E9] m-2">Your Favorites</Button>
         </Link>
         <Link to="/booking">
-          <Button>Your Bookings</Button>
+          <Button className="bg-[#3563E9] m-2">Your Bookings</Button>
         </Link>
       </div>
       {/*  <DropdownMenu>
